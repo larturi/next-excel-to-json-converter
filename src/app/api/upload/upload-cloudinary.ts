810cbe0f9c27ebaa45ddb0d5a1a6ca4acf158b12
Leandro Arturi <lea.arturi@gmail.com>
2024-04-17 20:00:19 -0300

    Add attribute upload_preset: excel_json_converter

--- src/app/api/upload/upload-cloudinary.ts
+++ src/app/api/upload/upload-cloudinary.ts
@@ -1,34 +1,34 @@
 import { cloudinary } from '@/app/config/config-cloudinary'
 
-export async function uploadCloudinaryByFile(file: any, public_id: string) {
-  try {
-    const arrayBuffer = await new Response(file).arrayBuffer()
-    const buffer = Buffer.from(arrayBuffer)
+export async function uploadCloudinaryByFile(file: any, public_id: string): Promise<string> {
+  const arrayBuffer = await new Response(file).arrayBuffer()
+  const buffer = Buffer.from(arrayBuffer)
+  const mime = file.type
+  const encoding = 'base64'
+  const base64Data = buffer.toString(encoding)
+  const fileUri = `data:${mime};${encoding},${base64Data}`
 
-    const response: any = await new Promise((resolve, reject) => {
-      cloudinary.uploader
-        .upload_stream(
-          {
-            folder: 'next-excel-to-json',
-            resource_type: 'raw',
-            upload_preset: 'excel_json_converter',
-            public_id: public_id
-          },
-          (err, result) => {
-            if (err) {
-              reject(err)
-            }
-            resolve(result)
-          }
-        )
-        .end(buffer)
-    })
-
-    return response.secure_url
-  } catch (error) {
-    console.log(error)
-    throw new Error('Error al intentar subir el archivo a Cloudinary')
-  }
+  return new Promise((resolve, reject) => {
+    cloudinary.uploader.upload(
+      fileUri,
+      {
+        folder: 'next-excel-to-json',
+        resource_type: 'raw',
+        upload_preset: 'excel_json_converter',
+        public_id: public_id,
+        invalidate: true
+      },
+      (error, result) => {
+        if (error) {
+          console.error(error)
+          reject(new Error('Error al intentar subir el archivo a Cloudinary'))
+        } else {
+          //  console.log(result)
+          resolve(result!.secure_url)
+        }
+      }
+    )
+  })
 }
 
 export async function uploadCloudinaryByUrl(fileUrl: string) {
